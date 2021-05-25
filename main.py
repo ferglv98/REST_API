a3acce22af745802078e527c9dda12fdee72d37a
Luis Fernando Garcia Luna V <fer_garcia_luna@hotmail.com>
2021-05-25 14:42:59 -0500

    Second commit

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+from flask import Flask
+from flask_restful import Api, Resource
 
-q
+app = Flask(__name__)
+api = Api(app)
+
+names = {'tim': {'age': 19, 'gender': 'male'},
+         'fer': {'age': 22, 'gender': 'male'},
+         'moni': {'age': 22, 'gender': 'female'}
+         }
+
+class HelloWorld(Resource):
+    def get(self, name):
+        return names[name]
+
+    def post(self):
+        return {"data": "Posted"}
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
+
+if __name__ == "__main__":
+    app.run(debug=True)             #True when develop mode, False else
+
